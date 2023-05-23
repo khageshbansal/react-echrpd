58b658fc4194403a90c9f0edbe5b8185f8e90ef6
khageshbansal <72146255+khageshbansal@users.noreply.github.com>
2023-05-23 23:13:15 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { BrowserRouter, Route, Routes, Outlet } from 'react-router-dom';
+import { Provider, useSelector } from 'react-redux';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import Navbar from './components/Navbar';
 import Shop from './components/Shop';
 import Footer from './components/Footer';
@@ -15,29 +16,27 @@ import Profile from './pages/Profile';
 import ProtectedRoute from './components/ProtectedRoute';
 import store from './redux/store';
 
+
+
 export default function App() {
-  const isAuthenticated = store.getState().auth.isLogged;
-  console.log(isAuthenticated);
+  
   return (
     <Provider store={store}>
+      
       <BrowserRouter>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/shop" element={<Shop />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<ContactUs />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
           <Route path="/forgot" element={<Forgot />} />
 
-          {isAuthenticated ? (
-            <>
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/shop" element={<Shop />} />
-            </>
-          ) : null}
-
           <Route path="/productDetails/:id" element={<ProductDetails />} />
+
           <Route path="*" element={<Home />} />
         </Routes>
         <Footer />
